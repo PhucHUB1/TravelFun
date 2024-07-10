a48664b31cbaacb895d9c46ecdc5ac3ac433bf64
phuch <phuchmth2209055@fpt.edu.vn>
2024-07-10 10:42:56 +0700

    create

---
+++ src/main/java/com/example/travelfun/TravelFunApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelFunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/travelfun/TravelFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
